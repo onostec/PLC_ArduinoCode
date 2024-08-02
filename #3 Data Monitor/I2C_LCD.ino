bc9acfba3d1bdada90386e96fc10de5295b307c5
OnosTech <78807522+onostec@users.noreply.github.com>
2024-08-02 14:21:36 +0100

    Add files via upload

---
+++ #3 Data Monitor/I2C_LCD.ino
@@ -0,0 +1,156 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+//Digital Input Declaration 
+const int s1 = A0;
+const int s2 = A1;
+const int s3 = A2;
+const int s4 = A3;
+const int s5 = 2;
+const int s6 = 3;
+const int s7 = 4;
+const int s8 = 5;
+
+//Digital Output Declaration
+const int r1 = 13;
+const int r2 = 12;
+const int r3 = 11;
+const int r4 = 6;
+const int r5 = 7;
+const int r6 = 8;
+const int r7 = 9;
+const int r8 = 10;
+
+//Analog Input Declaration
+const int a1 = A6;
+const int a2 = A7;
+
+int A_1;
+int A_2;
+
+//Inputs Boolean variable declaration
+bool S1;
+bool S2;
+bool S3;
+bool S4;
+bool S5;
+bool S6;
+bool S7;
+bool S8;
+
+//Outputs Boolean variable declaration
+bool R1;
+bool R2;
+bool R3;
+bool R4;
+bool R5;
+bool R6;
+bool R7;
+bool R8;
+
+int I1;
+
+void setup() {
+  IO(); // call the inputs and output pin configuration
+
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+
+  lcd.setCursor(0,0);              //display "ANALOG: " at column 0, row 0 of the LCD
+  lcd.print("ANALOG: ");
+  lcd.setCursor(0,1);              //display "LAMP: " at column 0, row 1 of the LCD
+  lcd.print("LAMP: ");
+
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600); 
+}
+
+void loop() {
+
+  input();
+
+    I1 = map(A_1, 0, 1023, 0, 99);
+    lcd.setCursor(8,0);              //display the value of I1 at column 8, row 0 of the LCD
+    lcd.print(I1);
+    
+    if (I1 >= 30 && I1 <= 80) {
+      R1 = 1;
+      lcd.setCursor(6,1);              //display ON at column 6, row 1 of the LCD
+      lcd.print("ON ");
+    } else {
+      R1 = 0;
+      lcd.setCursor(6,1);              //display ON at column 6, row 1 of the LCD
+      lcd.print("OFF");
+    }
+
+    Serial.println(I1);
+    Serial.println(R1);
+    delay(100);  
+
+  output();
+
+}
+
+//read input state
+void input() {
+S1 = !digitalRead(s1);
+S2 = !digitalRead(s2);
+S3 = !digitalRead(s3);
+S4 = !digitalRead(s4);
+S5 = !digitalRead(s5);
+S6 = !digitalRead(s6);
+S7 = !digitalRead(s7);
+S8 = !digitalRead(s8);
+
+A_1 = analogRead(a1);
+A_2 = analogRead(a2);
+}
+
+//Update output pin status
+void output() {
+  digitalWrite(r1, R1);  
+  digitalWrite(r2, R2);  
+  digitalWrite(r3, R3);  
+  digitalWrite(r4, R4);  
+  digitalWrite(r5, R5);  
+  digitalWrite(r6, R6);  
+  digitalWrite(r7, R7);  
+  digitalWrite(r8, R8);  
+}
+
+//configure input and output pins
+void IO() {
+//set pins to inputs 
+  pinMode(s1, INPUT);
+  pinMode(s2, INPUT);
+  pinMode(s3, INPUT);
+  pinMode(s4, INPUT);
+  pinMode(s5, INPUT);
+  pinMode(s6, INPUT);
+  pinMode(s7, INPUT);
+  pinMode(s8, INPUT);
+
+//set pins to outputs
+  pinMode(r1, OUTPUT);
+  pinMode(r2, OUTPUT);
+  pinMode(r3, OUTPUT);
+  pinMode(r4, OUTPUT);
+  pinMode(r5, OUTPUT);
+  pinMode(r6, OUTPUT);
+  pinMode(r7, OUTPUT);
+  pinMode(r8, OUTPUT);
+
+//Initialized the output pins to zero
+  digitalWrite(r1, false);  
+  digitalWrite(r2, false);  
+  digitalWrite(r3, false);  
+  digitalWrite(r4, false);  
+  digitalWrite(r5, false);  
+  digitalWrite(r6, false);  
+  digitalWrite(r7, false);  
+  digitalWrite(r8, false);  
+
+}
